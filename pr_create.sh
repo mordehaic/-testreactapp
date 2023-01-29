8f02f705b85ab4325e890543ac15760578a6e1d1
Mordehai Chicha <mordehai.chicha.ext@louisvuitton.com>
2023-01-29 12:29:56 +0200

    first init

---
+++ pr_create.sh
@@ -0,0 +1,22 @@
+# Add a comment
+BITBUCKET_REPO_OWNER="BalinkDev"                #louisvuitton_team
+BITBUCKET_REPO_SLUG="test_react_app"            #test_react_app
+USERNAME="mordehaic"
+PASSWORD="ATBBCw4Lyen4CWpTDYMqAey9Fdsa134DD8E8" # App passwords 
+BITBUCKET_BRANCH=${1:-'develop'}                          
+
+curl https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/pullrequests \
+-u ${USERNAME}:${PASSWORD}   \
+--request POST \
+-H 'Content-Type: application/json' \
+-d '{
+    "title": "Merge from '${BITBUCKET_BRANCH}' branch",
+    "description": "Automated PR creation process :-)", 
+    "source": {
+        "branch": {
+          "name": "'${BITBUCKET_BRANCH}'"
+        }
+    },
+    "close_source_branch": false,
+    "reviewers": '[]'
+}'
